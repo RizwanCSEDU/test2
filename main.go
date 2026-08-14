22daeaa9b829601b3d5919ecbbc23a2874bc0773
Md Rizwanul Haque <87943114+rizwanhrizvi@users.noreply.github.com>
2026-08-14 15:17:38 -0400

    Implement main function with sleep for gRPC
    Add main function with a sleep placeholder for gRPC handshake.

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "time"
+
+func main() {
+    // placeholder — terraform validate will invoke this binary
+    // and wait forever for the plugin gRPC handshake
+    time.Sleep(100)
+}
